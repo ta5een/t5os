5dac3e0ab4f5d9927ad16037769560b2a289016a
Ta-Seen <taseen00.islam@gmail.com>
2024-06-10 02:45:54 +1000

    feat: set up bootloader to kernel bridge with multiboot

---
+++ loader.s
@@ -0,0 +1,33 @@
+/**
+ * This source file functions as a bridge between the GRUB bootloader and the
+ * kernel. It sets up important header symbols so that the bootloader can
+ * detect this custom kernel. Additionally, it sets up a 2MiB stack before
+ * jumping to the `kmain` function in the kernel.
+ */
+
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)       # Magic number for GRUB bootloader
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .text
+.extern kmain                   # `kmain` is defined in `kernel.cpp`
+
+.global loader                  # Entry point for the linker
+loader:
+  mov $stack, %esp              # Set up the kernel stack
+  push %eax                     # Push `AX` register (multiboot info) to stack
+  push %ebx                     # Push `BX` register (magic number) to stack
+  call kmain                    # Hand-over to C++
+_loop:                          # Infinite loop in case we exit out of C++
+  cli
+  hlt
+  jmp _loop
+
+.section .bss
+.space 2*1024*1024              # Add 2MiB padding
+stack:                          # Will point to some available space in RAM
