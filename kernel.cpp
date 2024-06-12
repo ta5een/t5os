0d2b80068d0e3c5211cc3f927aeb1cf02a6a3b8d
Ta-Seen <taseen00.islam@gmail.com>
2024-06-13 02:05:32 +1000

    chore: update formatting style

--- kernel.cpp
+++ kernel.cpp
@@ -1,52 +1,52 @@
-using u8 = unsigned char;
-using u16 = unsigned short;
-using u32 = unsigned int;
-using u64 = unsigned long long;
+using U8 = unsigned char;
+using U16 = unsigned short;
+using U32 = unsigned int;
+using U64 = unsigned long long;
 
-using s8 = signed char;
-using s16 = signed short;
-using s32 = signed int;
-using s64 = signed long long;
+using S8 = signed char;
+using S16 = signed short;
+using S32 = signed int;
+using S64 = signed long long;
 
-using multiboot_t = void *;
-using constructor_t = void (*)();
+using Multiboot = void *;
+using Constructor = void (*)();
 
-enum vga_color
+enum class VgaColor : U8
 {
-  VGA_COLOR_BLACK = 0x0,
-  VGA_COLOR_BLUE = 0x1,
-  VGA_COLOR_GREEN = 0x2,
-  VGA_COLOR_CYAN = 0x3,
-  VGA_COLOR_RED = 0x4,
-  VGA_COLOR_MAGENTA = 0x5,
-  VGA_COLOR_BROWN = 0x6,
-  VGA_COLOR_LIGHT_GREY = 0x7,
-  VGA_COLOR_DARK_GREY = 0x8,
-  VGA_COLOR_LIGHT_BLUE = 0x9,
-  VGA_COLOR_LIGHT_GREEN = 0xA,
-  VGA_COLOR_LIGHT_CYAN = 0xB,
-  VGA_COLOR_LIGHT_RED = 0xC,
-  VGA_COLOR_LIGHT_MAGENTA = 0xD,
-  VGA_COLOR_LIGHT_BROWN = 0xE,
-  VGA_COLOR_WHITE = 0xF,
+	Black = 0x0,
+	Blue = 0x1,
+	Green = 0x2,
+	Cyan = 0x3,
+	Red = 0x4,
+	Magenta = 0x5,
+	Brown = 0x6,
+	LightGrey = 0x7,
+	DarkGrey = 0x8,
+	LightBlue = 0x9,
+	LightGreen = 0xA,
+	LightCyan = 0xB,
+	LightRed = 0xC,
+	LightMegenta = 0xD,
+	LightBrown = 0xE,
+	White = 0xF,
 };
 
-u16 *const VGA_MEMORY = (u16 *)0xB8000;
-u8 const VGA_SCREEN_COLOR = VGA_COLOR_WHITE | (VGA_COLOR_BLACK << 4);
+U16 *const VGA_MEMORY = (U16 *)0xB8000;
+const U8 VGA_SCREEN_COLOR = (U8)VgaColor::White | ((U8)VgaColor::Black << 4);
 
-extern "C" constructor_t start_ctors;
-extern "C" constructor_t end_ctors;
+extern "C" Constructor start_ctors;
+extern "C" Constructor end_ctors;
 
 /**
  * Output a white-on-black character to the VGA memory buffer.
  */
 void vga_write_to_buffer(const char *str)
 {
-  for (u8 i = 0; str[i] != '\0'; i++)
-  {
-    // Set the highest bits to the default foreground and background
-    VGA_MEMORY[i] = (u16)str[i] | (u16)(VGA_SCREEN_COLOR) << 8;
-  }
+	for (U8 i = 0; str[i] != '\0'; i++)
+	{
+		// Set the highest bits to the default foreground and background
+		VGA_MEMORY[i] = (U16)str[i] | (U16)(VGA_SCREEN_COLOR) << 8;
+	}
 }
 
 /**
@@ -54,20 +54,20 @@ void vga_write_to_buffer(const char *str)
  */
 extern "C" void call_ctors()
 {
-  for (constructor_t *ctor = &start_ctors; ctor != &end_ctors; ctor++)
-  {
-    (*ctor)();
-  }
+	for (Constructor *ctor = &start_ctors; ctor != &end_ctors; ctor++)
+	{
+		(*ctor)();
+	}
 }
 
 /**
  * Entry point of the kernel.
  */
-extern "C" void kmain(multiboot_t multiboot, u32 magic)
+extern "C" void kmain(Multiboot multiboot, U32 magic)
 {
-  vga_write_to_buffer("Hello, world!");
-  while (true)
-  {
-    // Infinite loop
-  }
+	vga_write_to_buffer("Hello, world!");
+	while (true)
+	{
+		// Infinite loop
+	}
 }
