131b8ef9370c0168b3ebe436253e47d3cf098fe0
Ta-Seen <taseen00.islam@gmail.com>
2024-06-15 05:20:18 +1000

    chore: revert indent style to spaces

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -6,22 +6,22 @@ using Constructor = void (*)();
 
 enum class VgaColor : u8
 {
-	Black = 0x0,
-	Blue = 0x1,
-	Green = 0x2,
-	Cyan = 0x3,
-	Red = 0x4,
-	Magenta = 0x5,
-	Brown = 0x6,
-	LightGrey = 0x7,
-	DarkGrey = 0x8,
-	LightBlue = 0x9,
-	LightGreen = 0xA,
-	LightCyan = 0xB,
-	LightRed = 0xC,
-	LightMegenta = 0xD,
-	LightBrown = 0xE,
-	White = 0xF,
+    Black = 0x0,
+    Blue = 0x1,
+    Green = 0x2,
+    Cyan = 0x3,
+    Red = 0x4,
+    Magenta = 0x5,
+    Brown = 0x6,
+    LightGrey = 0x7,
+    DarkGrey = 0x8,
+    LightBlue = 0x9,
+    LightGreen = 0xA,
+    LightCyan = 0xB,
+    LightRed = 0xC,
+    LightMegenta = 0xD,
+    LightBrown = 0xE,
+    White = 0xF,
 };
 
 u16 *const VGA_MEMORY = (u16 *)0xB8000;
@@ -35,11 +35,11 @@ extern "C" Constructor end_ctors;
  */
 void vga_write_to_buffer(const char *str)
 {
-	for (u8 i = 0; str[i] != '\0'; i++)
-	{
-		// Set the highest bits to the default foreground and background
-		VGA_MEMORY[i] = (u16)str[i] | (u16)(VGA_SCREEN_COLOR) << 8;
-	}
+    for (u8 i = 0; str[i] != '\0'; i++)
+    {
+        // Set the highest bits to the default foreground and background
+        VGA_MEMORY[i] = (u16)str[i] | (u16)(VGA_SCREEN_COLOR) << 8;
+    }
 }
 
 /**
@@ -47,10 +47,10 @@ void vga_write_to_buffer(const char *str)
  */
 extern "C" void call_ctors()
 {
-	for (Constructor *ctor = &start_ctors; ctor != &end_ctors; ctor++)
-	{
-		(*ctor)();
-	}
+    for (Constructor *ctor = &start_ctors; ctor != &end_ctors; ctor++)
+    {
+        (*ctor)();
+    }
 }
 
 /**
@@ -58,16 +58,16 @@ extern "C" void call_ctors()
  */
 extern "C" void _kmain(Multiboot /* multiboot */, u32 /* magic */)
 {
-	const char *welcome_msg;
-	if (KERNEL_ARCH_IS_I386())
-	{
-		welcome_msg = "Welcome to WYOOS (arch=i386)";
-	}
+    const char *welcome_msg;
+    if (KERNEL_ARCH_IS_I386())
+    {
+        welcome_msg = "Welcome to WYOOS (arch=i386)";
+    }
 
-	vga_write_to_buffer(welcome_msg);
+    vga_write_to_buffer(welcome_msg);
 
-	while (true)
-	{
-		// Infinite loop
-	}
+    while (true)
+    {
+        // Infinite loop
+    }
 }
--- src/lib/integers.hpp
+++ src/lib/integers.hpp
@@ -6,15 +6,15 @@
 
 namespace lib
 {
-	using u8 = unsigned char;
-	using u16 = unsigned short;
-	using u32 = unsigned int;
-	using u64 = unsigned long long;
+    using u8 = unsigned char;
+    using u16 = unsigned short;
+    using u32 = unsigned int;
+    using u64 = unsigned long long;
 
-	using s8 = signed char;
-	using s16 = signed short;
-	using s32 = signed int;
-	using s64 = signed long long;
+    using s8 = signed char;
+    using s16 = signed short;
+    using s32 = signed int;
+    using s64 = signed long long;
 } // namespace lib
 
 #if USING_LIB_GLOBALLY
