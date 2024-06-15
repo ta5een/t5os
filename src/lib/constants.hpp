66287e857a301f6ec6c3166287196cddf19900cd
Ta-Seen <taseen00.islam@gmail.com>
2024-06-16 01:44:24 +1000

    chore(lib): add header file for constants

---
+++ src/lib/constants.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <lib/integers.hpp>
+
+namespace lib
+{
+    constexpr u64 KiB = 1024;
+    constexpr u64 MiB = KiB * KiB;
+    constexpr u64 GiB = KiB * KiB * KiB;
+} // namespace lib
