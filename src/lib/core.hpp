a12463a99fdfe8c7f5bff21b8cc06a124934b8d9
Ta-Seen <taseen00.islam@gmail.com>
2024-06-18 00:27:06 +1000

    feat(lib): define `usize` and `ssize` data types

---
+++ src/lib/core.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#ifndef USING_LIB_GLOBALLY
+#    define USING_LIB_GLOBALLY 1
+#endif
--- src/lib/integers.hpp
+++ src/lib/integers.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#ifndef USING_LIB_GLOBALLY
-#    define USING_LIB_GLOBALLY 1
-#endif
+#include <lib/core.hpp>
 
 namespace lib
 {
@@ -15,16 +13,30 @@ namespace lib
     using s16 = signed short;
     using s32 = signed int;
     using s64 = signed long long;
+
+    // Taken from:
+    // https://awesomekling.github.io/How-SerenityOS-declares-ssize_t/
+    using usize = __SIZE_TYPE__;
+#define unsigned signed
+    using ssize = __SIZE_TYPE__;
+#undef unsigned
+
 } // namespace lib
 
 #if USING_LIB_GLOBALLY
+// clang-format off
+using lib::u8;
 using lib::u16;
 using lib::u32;
 using lib::u64;
-using lib::u8;
+using lib::usize;
+// clang-format on
 
+// clang-format off
+using lib::s8;
 using lib::s16;
 using lib::s32;
 using lib::s64;
-using lib::s8;
+using lib::ssize;
+// clang-format on
 #endif
