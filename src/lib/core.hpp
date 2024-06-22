e45bef9c45bd9749f333120614e5213d4366fb2a
Ta-Seen <taseen00.islam@gmail.com>
2024-06-22 19:11:00 +1000

    feat(lib): add macros to delete copy and move constructors

--- src/lib/core.hpp
+++ src/lib/core.hpp
@@ -3,3 +3,13 @@
 #ifndef USING_LIB_GLOBALLY
 #    define USING_LIB_GLOBALLY 1
 #endif
+
+#define LIB_MAKE_NONCOPYABLE(T)                                                \
+  private:                                                                     \
+    T(const T &) = delete;                                                     \
+    T &operator=(const T &) = delete;
+
+#define LIB_MAKE_NONMOVABLE(T)                                                 \
+  private:                                                                     \
+    T(T &&) = delete;                                                          \
+    T &operator=(T &&) = delete;
