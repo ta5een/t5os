ec79bab80184b010b6fffe483adabc9823f65017
Ta-Seen <taseen00.islam@gmail.com>
2024-06-29 20:52:50 +1000

    feat(scripts): add flag to rebuild Docker image

--- scripts/docker-build-image.sh
+++ scripts/docker-build-image.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+print_usage() {
+  printf "Usage: %s [-rh]\n" "$(basename "$0")"
+  echo "Build Docker image 'wyoos', using the Dockerfile in project root"
+  echo ""
+  echo "Options:"
+  echo "  -r: Rebuild the image from scratch, as opposed to from cache"
+}
+
+rebuild=false
+while getopts "rh" flag; do
+  case "$flag" in
+    r) rebuild=true ;;
+    h) print_usage; exit 0 ;;
+    *) print_usage; exit 1 ;;
+  esac
+done
+
 # Check if Docker is available
 hash docker 2>/dev/null || {
   echo "ERROR: 'docker' is not available" >&2
@@ -9,4 +26,8 @@ hash docker 2>/dev/null || {
 }
 
 # Build the Dockerfile (in project root) with the tag "wyoos"
-docker build -t wyoos .
+if [ "$rebuild" = true ]; then
+  docker build --no-cache -t wyoos .
+else
+  docker build -t wyoos .
+fi
