98a3f0d454c4f65c69b4f5bb033a4989593f14cc
Ta-Seen <taseen00.islam@gmail.com>
2024-07-08 01:52:03 +1000

    chore(kernel): guard use of HLT instruction to only x86 targets

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -19,9 +19,13 @@ void kmain(
     // Idle loop
     while (true)
     {
+#if defined(__i386__) || defined(__x86_64__)
         // Use the hlt instruction to put the CPU in a low-power state when
         // idle. This reduces CPU usage and power consumption compared to a
         // busy-wait loop.
         asm volatile("hlt");
+#else
+    #warning "Idle loop not defined for current architecture"
+#endif
     }
 }
