83771d32c4f309aaf313500edb134bedf43afb8d
Ta-Seen <taseen00.islam@gmail.com>
2024-07-28 04:49:13 +1000

    inline `arch_cpu_halt` and `arch_cpu_idle_loop`

--- kernel/src/arch/x86/cpu.c
+++ kernel/src/arch/x86/cpu.c
@@ -60,24 +60,3 @@ arch_cpu_init()
     asm volatile("int $0x1f");
     vga_println(vga, " [DONE]");
 }
-
-void
-arch_cpu_halt()
-{
-    while (true)
-    {
-        asm volatile("cli; hlt");
-    }
-}
-
-void
-arch_cpu_idle_loop()
-{
-    while (true)
-    {
-        // Use the hlt instruction to put the CPU in a low-power state when
-        // idle. This reduces CPU usage and power consumption compared to a
-        // busy-wait loop.
-        asm volatile("hlt");
-    }
-}
