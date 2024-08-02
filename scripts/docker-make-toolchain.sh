12c41ce0e4768e24c9a5ee7b761c9d01d1551a77
Ta-Seen <taseen00.islam@gmail.com>
2024-08-03 01:06:59 +1000

    add script to build toolchain in Docker

---
+++ scripts/docker-make-toolchain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Check if Docker is available
+hash docker 2>/dev/null || {
+  echo "ERROR: 'docker' is not available" >&2
+  exit 1
+}
+
+# With the Docker image "t5os", in an ephemeral container, build the toolchain
+docker run -it --rm -v .:/t5os t5os make toolchain
