a87c2678fd4b1d0d844085bd80f71122e154bf68
Ta-Seen <taseen00.islam@gmail.com>
2024-08-11 04:15:28 +1000

    reimplement unit test in Criterion

--- kernel/test/log/test_log_debug.c
+++ kernel/test/log/test_log_debug.c
@@ -1,22 +1,10 @@
-#include "kernel/log.h"
-#include <assert.h>
+#include <criterion/criterion.h>
 #include <stddef.h>
-#include <stdio.h>
 #include <string.h>
-
-#define TEST_EXIT_PASS 0
-#define TEST_EXIT_SKIP 77
-#define TEST_EXIT_FAIL 99
 
 #define BUFFER_SIZE 256
 
 char buffer[BUFFER_SIZE] = {0};
-
-static void
-cleanup()
-{
-    memset(buffer, 0, BUFFER_SIZE);
-}
 
 static size_t
 write(const char *str, size_t str_len)
@@ -26,36 +14,11 @@ write(const char *str, size_t str_len)
     return n_bytes;
 }
 
-int
-main(void)
+Test(kernel, log_debug)
 {
-    struct log_spec spec = {.write = write};
-    (void)spec;
-
-    {
-        cleanup();
-        const char str[] = "Hello, world!";
-        const size_t str_len = 14;
-        size_t n_bytes = write(str, str_len);
-        if (n_bytes != str_len)
-        {
-            fprintf(stderr, "nbytes: expected %zu, got %zu", str_len, n_bytes);
-            return TEST_EXIT_FAIL;
-        }
-    }
-
-    /* {
-        cleanup();
-        const char str[] = "Hello, world!";
-        const size_t str_len = 14;
-        write(str, str_len);
-        if (buffer[0] != str[0])
-        {
-            fprintf(stderr, "buffer[0]: expected %c, got %c", str[0],
-                    buffer[0]);
-            return TEST_EXIT_FAIL;
-        }
-    } */
-
-    return TEST_EXIT_PASS;
+    const char str[] = "Hello, world!";
+    size_t str_len = strlen(str);
+    size_t n_bytes = write(str, str_len);
+    cr_assert(n_bytes == str_len, "nbytes: expected %zu, got %zu", str_len,
+              n_bytes);
 }
