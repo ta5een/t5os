5b7cbb4f729a05056ca8be19270de9d8cdb05528
Ta-Seen <taseen00.islam@gmail.com>
2024-08-22 20:28:48 +1000

    make use of conformant array parameters

--- libt5/src/log.c
+++ libt5/src/log.c
@@ -2,15 +2,17 @@
 #include <stdarg.h>
 
 void
-log_trace(struct log_spec *spec, const char *location, const char *fmt, ...)
+log_trace(struct log_spec *spec, const char *restrict loc,
+          const char *restrict fmt, ...)
 {
-    (void)location;
+    (void)loc;
     (void)fmt;
 
     va_list list;
     va_start(list, fmt);
 
-    spec->write("Hello, world!", 14);
+    const char str_hello[] = "Hello, world!";
+    spec->write(sizeof(str_hello), str_hello);
 
     va_end(list);
 }
