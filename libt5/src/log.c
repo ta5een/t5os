d6b5a7d5c0e29abb77d0c57df4ff5caaf9c04234
Ta-Seen <taseen00.islam@gmail.com>
2024-08-30 02:09:31 +1000

    rename `log_spec.ctx` -> `log_spec.context`

--- libt5/src/log.c
+++ libt5/src/log.c
@@ -14,7 +14,9 @@ log_trace(struct log_spec *spec, const char *restrict loc,
     // TODO: Use of hard-coded string
     const char str_hello[] = "Hello, world!";
     // TODO: sizeof is used here since strlen has not been implemented yet
-    spec->write(sizeof(str_hello), str_hello, spec->ctx);
+    size_t bytes = spec->write(sizeof(str_hello), str_hello, spec->context);
+    // TODO: What to do with the remaining bytes?
+    (void)bytes;
 
     va_end(list);
 }
